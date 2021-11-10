6f7ac13b1ba7b386ca637937f64fbb1628b0ab6a
Henry Burgess <henryjburg@gmail.com>
2021-11-10 17:15:25 -0600

    Initial styling prototype

--- src/lib/graphics/components/Choice.tsx
+++ src/lib/graphics/components/Choice.tsx
@@ -51,177 +51,177 @@ export function Choice(props: {
   }
 
   return (
-    <div>
-      <Grid
-        rows={['flex', 'flex']}
-        columns={['flex', 'large', 'flex']}
-        gap='small'
-        areas={[
-          {name: 'playerArea', start: [0, 0], end: [0, 1]},
-          {name: 'choiceOneArea', start: [1, 0], end: [1, 0]},
-          {name: 'choiceTwoArea', start: [1, 1], end: [1, 1]},
-          {name: 'partnerArea', start: [2, 0], end: [2, 1]},
-        ]}
-        responsive
+    <Grid
+      rows={['flex', 'flex']}
+      columns={['flex', 'large', 'flex']}
+      gap='small'
+      areas={[
+        {name: 'playerArea', start: [0, 0], end: [0, 1]},
+        {name: 'choiceOneArea', start: [1, 0], end: [1, 0]},
+        {name: 'choiceTwoArea', start: [1, 1], end: [1, 1]},
+        {name: 'partnerArea', start: [2, 0], end: [2, 1]},
+      ]}
+      responsive
+    >
+      {/* Participant's Avatar */}
+      <Box
+        gridArea='playerArea'
+        background='brand'
+        round
+        justify='center'
+        align='center'
+        direction='row-responsive'
+        id='participantInfo'
+        margin={{right: 'small'}}
       >
-        {/* Participant's Avatar */}
-        <Box
-          gridArea='playerArea'
-          background='brand'
-          round
-          justify='center'
-          align='center'
-          direction='row-responsive'
-          id='participantInfo'
-          margin={{right: 'small'}}
+        <Grid
+          rows={['small', 'flex', 'small']}
+          columns={['flex']}
+          gap='xsmall'
+          areas={[
+            {name: 'youNameArea', start: [0, 0], end: [0, 0]},
+            {name: 'youAvatarArea', start: [0, 1], end: [0, 1]},
+            {name: 'youPointsArea', start: [0, 2], end: [0, 2]},
+          ]}
+          pad='small'
         >
-          <Grid
-            rows={['small', 'flex', 'small']}
-            columns={['flex']}
-            gap='xsmall'
-            areas={[
-              {name: 'youNameArea', start: [0, 0], end: [0, 0]},
-              {name: 'youAvatarArea', start: [0, 1], end: [0, 1]},
-              {name: 'youPointsArea', start: [0, 2], end: [0, 2]},
-            ]}
-            pad='small'
+          <Box
+            align='center'
+            animation={['pulse']}
+            gridArea='youAvatarArea'
+            alignSelf='center'
           >
-            <Box
-              align='center'
-              animation={['pulse']}
-              gridArea='youAvatarArea'
-              alignSelf='center'
-            >
-              {getStyledAvatar(`${config.avatars[props.avatar - 1]}`, 128)}
-            </Box>
-            <Box align='center' gridArea='youNameArea' alignSelf='center'>
-              <Heading>You</Heading>
-            </Box>
-            <Box align='center' gridArea='youPointsArea' alignSelf='center'>
-              <Heading level={2}>
-                Points:&nbsp;
-                <TextTransition
-                  text={participantPoints}
-                  springConfig={presets.wobbly}
-                  inline={true}
-                />
-              </Heading>
-            </Box>
-          </Grid>
-        </Box>
+            {getStyledAvatar(`${config.avatars[props.avatar - 1]}`, 128)}
+          </Box>
+          <Box align='center' gridArea='youNameArea' alignSelf='center'>
+            <Heading>You</Heading>
+          </Box>
+          <Box align='center' gridArea='youPointsArea' alignSelf='center'>
+            <Heading level={2}>
+              Points:&nbsp;
+              <TextTransition
+                text={participantPoints}
+                springConfig={presets.wobbly}
+                inline={true}
+              />
+            </Heading>
+          </Box>
+        </Grid>
+      </Box>
 
-        <Box
-          gridArea='choiceOneArea'
-          background='light-5'
-          ref={optionOneRef}
-          onClick={() => {
-            updatePointsDisplay(
-                props.rowData.Option1_PPT,
-                props.rowData.Option1_Partner
-            );
-            const optionOneNode = optionOneRef.current as HTMLElement;
-            const optionTwoNode = optionTwoRef.current as HTMLElement;
-            optionOneNode.style.opacity = '0';
-            optionTwoNode.style.opacity = '0';
-            optionOneNode.style.pointerEvents = 'none';
-            optionTwoNode.style.pointerEvents = 'none';
-            window.setTimeout(() => {
-              props.callback('optionOne');
-              optionOneNode.style.opacity = '1';
-              optionTwoNode.style.opacity = '1';
-              optionOneNode.style.pointerEvents = 'auto';
-              optionTwoNode.style.pointerEvents = 'auto';
-            }, 1000);
-          }}
-          className='grow'
-        >
-          <Option
-            optionKey='optionOne'
-            optionName='Option 1'
-            pointsPPT={props.rowData.Option1_PPT}
-            pointsParter={props.rowData.Option1_Partner}
-          />
-        </Box>
+      <Box
+        gridArea='choiceOneArea'
+        ref={optionOneRef}
+        onClick={() => {
+          updatePointsDisplay(
+              props.rowData.Option1_PPT,
+              props.rowData.Option1_Partner
+          );
+          const optionOneNode = optionOneRef.current as HTMLElement;
+          const optionTwoNode = optionTwoRef.current as HTMLElement;
+          optionOneNode.style.opacity = '0';
+          optionTwoNode.style.opacity = '0';
+          optionOneNode.style.pointerEvents = 'none';
+          optionTwoNode.style.pointerEvents = 'none';
+          window.setTimeout(() => {
+            props.callback('optionOne');
+            optionOneNode.style.opacity = '1';
+            optionTwoNode.style.opacity = '1';
+            optionOneNode.style.pointerEvents = 'auto';
+            optionTwoNode.style.pointerEvents = 'auto';
+          }, 1000);
+        }}
+        className='grow'
+        round
+        background='neutral-3'
+      >
+        <Option
+          optionKey='optionOne'
+          optionName='Option 1'
+          pointsPPT={props.rowData.Option1_PPT}
+          pointsParter={props.rowData.Option1_Partner}
+        />
+      </Box>
 
-        <Box
-          gridArea='choiceTwoArea'
-          background='light-5'
-          ref={optionTwoRef}
-          onClick={() => {
-            updatePointsDisplay(
-                props.rowData.Option2_PPT,
-                props.rowData.Option2_Partner
-            );
-            const optionOneNode = optionOneRef.current as HTMLElement;
-            const optionTwoNode = optionTwoRef.current as HTMLElement;
-            optionOneNode.style.opacity = '0';
-            optionTwoNode.style.opacity = '0';
-            optionOneNode.style.pointerEvents = 'none';
-            optionTwoNode.style.pointerEvents = 'none';
-            window.setTimeout(() => {
-              props.callback('optionTwo');
-              optionOneNode.style.opacity = '1';
-              optionTwoNode.style.opacity = '1';
-              optionOneNode.style.pointerEvents = 'auto';
-              optionTwoNode.style.pointerEvents = 'auto';
-            }, 1000);
-          }}
-          className='grow'
-        >
-          <Option
-            optionKey='optionTwo'
-            optionName='Option 2'
-            pointsPPT={props.rowData.Option2_PPT}
-            pointsParter={props.rowData.Option2_Partner}
-          />
-        </Box>
+      <Box
+        gridArea='choiceTwoArea'
+        ref={optionTwoRef}
+        onClick={() => {
+          updatePointsDisplay(
+              props.rowData.Option2_PPT,
+              props.rowData.Option2_Partner
+          );
+          const optionOneNode = optionOneRef.current as HTMLElement;
+          const optionTwoNode = optionTwoRef.current as HTMLElement;
+          optionOneNode.style.opacity = '0';
+          optionTwoNode.style.opacity = '0';
+          optionOneNode.style.pointerEvents = 'none';
+          optionTwoNode.style.pointerEvents = 'none';
+          window.setTimeout(() => {
+            props.callback('optionTwo');
+            optionOneNode.style.opacity = '1';
+            optionTwoNode.style.opacity = '1';
+            optionOneNode.style.pointerEvents = 'auto';
+            optionTwoNode.style.pointerEvents = 'auto';
+          }, 1000);
+        }}
+        className='grow'
+        round
+        background='neutral-3'
+      >
+        <Option
+          optionKey='optionTwo'
+          optionName='Option 2'
+          pointsPPT={props.rowData.Option2_PPT}
+          pointsParter={props.rowData.Option2_Partner}
+        />
+      </Box>
 
-        {/* Partner's Avatar */}
-        <Box
-          gridArea='partnerArea'
-          background='neutral-1'
-          round
-          justify='center'
-          align='center'
-          direction='row-responsive'
-          id='partnerInfo'
-          margin={{left: 'small'}}
+      {/* Partner's Avatar */}
+      <Box
+        gridArea='partnerArea'
+        background='neutral-1'
+        round
+        justify='center'
+        align='center'
+        direction='row-responsive'
+        id='partnerInfo'
+        margin={{left: 'small'}}
+      >
+        <Grid
+          rows={['small', 'flex', 'small']}
+          columns={['flex']}
+          gap='xsmall'
+          areas={[
+            {name: 'friendNameArea', start: [0, 0], end: [0, 0]},
+            {name: 'friendAvatarArea', start: [0, 1], end: [0, 1]},
+            {name: 'friendPointsArea', start: [0, 2], end: [0, 2]},
+          ]}
+          pad='small'
         >
-          <Grid
-            rows={['small', 'flex', 'small']}
-            columns={['flex']}
-            gap='xsmall'
-            areas={[
-              {name: 'friendNameArea', start: [0, 0], end: [0, 0]},
-              {name: 'friendAvatarArea', start: [0, 1], end: [0, 1]},
-              {name: 'friendPointsArea', start: [0, 2], end: [0, 2]},
-            ]}
-            pad='small'
+          <Box
+            align='center'
+            animation={['pulse']}
+            gridArea='friendAvatarArea'
+            alignSelf='center'
           >
-            <Box
-              align='center'
-              animation={['pulse']}
-              gridArea='friendAvatarArea'
-              alignSelf='center'
-            >
-              {getStyledAvatar(`eeee`, 128)}
-            </Box>
-            <Box align='center' gridArea='friendNameArea' alignSelf='center'>
-              <Heading>Friend</Heading>
-            </Box>
-            <Box align='center' gridArea='friendPointsArea' alignSelf='center'>
-              <Heading level={2}>
-                Points:&nbsp;
-                <TextTransition
-                  text={partnerPoints}
-                  springConfig={presets.wobbly}
-                  inline={true}
-                />
-              </Heading>
-            </Box>
-          </Grid>
-        </Box>
-      </Grid>
-    </div>
+            {getStyledAvatar(`eeee`, 128)}
+          </Box>
+          <Box align='center' gridArea='friendNameArea' alignSelf='center'>
+            <Heading>Friend</Heading>
+          </Box>
+          <Box align='center' gridArea='friendPointsArea' alignSelf='center'>
+            <Heading level={2}>
+              Points:&nbsp;
+              <TextTransition
+                text={partnerPoints}
+                springConfig={presets.wobbly}
+                inline={true}
+              />
+            </Heading>
+          </Box>
+        </Grid>
+      </Box>
+    </Grid>
   );
 }
--- src/lib/graphics/components/Option.tsx
+++ src/lib/graphics/components/Option.tsx
@@ -16,41 +16,45 @@ export function Option(props: {
   pointsParter: number,
 }): ReactElement {
   return (
-    <div>
+    <Grid
+      rows={['flex', 'flex']}
+      columns={['flex', 'flex']}
+      gap='small'
+      areas={[
+        {name: 'choiceHeader', start: [0, 0], end: [1, 0]},
+        {name: 'pointsYou', start: [0, 1], end: [0, 1]},
+        {name: 'pointsPartner', start: [1, 1], end: [1, 1]},
+      ]}
+      id={props.optionKey}
+      fill='vertical'
+    >
+      {/* Choice header */}
       <Box
+        gridArea='choiceHeader'
         justify='center'
         alignContent='center'
         fill='horizontal'
       >
-        <Heading level={3} fill>{props.optionName}</Heading>
+        <Heading level={2} fill>{props.optionName}</Heading>
       </Box>
-      <Grid
-        rows={['flex']}
-        columns={['flex', 'flex']}
-        gap='small'
-        areas={[
-          {name: 'pointsYou', start: [0, 0], end: [0, 0]},
-          {name: 'pointsPartner', start: [1, 0], end: [1, 0]},
-        ]}
-        id={props.optionKey}
-        fill='vertical'
-      >
-        <Box
-          gridArea='pointsYou'
-          justify='center'
-          alignSelf='center'
-        >
-          <Heading level={3}>{props.pointsPPT}</Heading>
-        </Box>
 
-        <Box
-          gridArea='pointsPartner'
-          justify='center'
-          alignSelf='center'
-        >
-          <Heading level={3}>{props.pointsParter}</Heading>
-        </Box>
-      </Grid>
-    </div>
+      {/* Participant points */}
+      <Box
+        gridArea='pointsYou'
+        justify='center'
+        alignSelf='center'
+      >
+        <Heading level={2}>{props.pointsPPT}</Heading>
+      </Box>
+
+      {/* Partner points */}
+      <Box
+        gridArea='pointsPartner'
+        justify='center'
+        alignSelf='center'
+      >
+        <Heading level={2}>{props.pointsParter}</Heading>
+      </Box>
+    </Grid>
   );
 }
