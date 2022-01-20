86ebc3d153599059aef5ba73e9bbb4d02d7c422b
Henry Burgess <henryjburg@gmail.com>
2022-01-20 09:58:03 -0600

    Code clean-up

--- src/types/screens.d.ts
+++ src/types/screens.d.ts
@@ -30,7 +30,7 @@ declare namespace Screens {
 
   interface SelectAvatar {
     display: Display;
-    selectionHandler: (_selection: string) => void;
+    selectionHandler: (selection: string) => void;
   }
 
   interface Inference {
