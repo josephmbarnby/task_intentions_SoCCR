6c530b71e190cdf70f11a6257bdf0e56e4dab7c4
Henry Burgess <henryjburg@gmail.com>
2022-02-08 09:42:55 -0600

    Extract data from completed 'playerChoice' trials

--- src/task/lib/view/screens/Matching/index.tsx
+++ src/task/lib/view/screens/Matching/index.tsx
@@ -23,84 +23,39 @@ const Matching = (props: Screens.Matching): ReactElement => {
 
   // Launch request
   if (props.fetchData) {
+    // Collate data from 'playerChoice' trials
+    consola.info(`Collating data...`);
+    const dataCollection =
+        jsPsych.data.get()
+            .filter({
+              display: 'playerChoice',
+            })
+            .values();
+    consola.debug(
+        `'dataCollection' containing trials with 'display' = 'playerChoice':`,
+        dataCollection
+    );
+    const requestResponses = [];
+    for (const row of dataCollection) {
+      requestResponses.push({
+        ID: 'NA',
+        Trial: row.trial,
+        ppt1: row.playerPoints_option1,
+        par1: row.partnerPoints_option1,
+        ppt2: row.playerPoints_option2,
+        par2: row.partnerPoints_option2,
+        Ac: row.selectedOption_player,
+        Phase: 1,
+      });
+    }
+    consola.debug(`Request content 'requestResponses':`, requestResponses);
+
+    // Launch request to endpoint
     consola.info(`Requesting partner...`);
     axios.get(Configuration.endpoint, {
       params: {
         id: 48294,
-        responses: JSON.stringify([
-          {ID: 'NA', Trial: 1, ppt1: 6, par1: 6, ppt2: 10, par2: 6,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 2, ppt1: 7, par1: 7, ppt2: 10, par2: 7,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 3, ppt1: 8, par1: 8, ppt2: 10, par2: 8,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 4, ppt1: 6, par1: 6, ppt2: 6, par2: 2,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 5, ppt1: 7, par1: 7, ppt2: 7, par2: 2,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 6, ppt1: 8, par1: 8, ppt2: 8, par2: 2,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 7, ppt1: 6, par1: 2, ppt2: 8, par2: 6,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 8, ppt1: 7, par1: 2, ppt2: 8, par2: 6,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 9, ppt1: 8, par1: 2, ppt2: 9, par2: 6,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 10, ppt1: 12, par1: 8, ppt2: 8, par2: 8,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 11, ppt1: 12, par1: 9, ppt2: 9, par2: 9,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 12, ppt1: 12, par1: 10, ppt2: 10, par2: 10,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 13, ppt1: 8, par1: 5, ppt2: 8, par2: 8,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 14, ppt1: 9, par1: 5, ppt2: 9, par2: 9,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 15, ppt1: 10, par1: 5, ppt2: 10, par2: 10,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 16, ppt1: 10, par1: 6, ppt2: 8, par2: 2,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 17, ppt1: 11, par1: 6, ppt2: 9, par2: 2,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 18, ppt1: 12, par1: 6, ppt2: 10, par2: 2,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 19, ppt1: 4, par1: 4, ppt2: 8, par2: 4,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 20, ppt1: 5, par1: 5, ppt2: 8, par2: 5,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 21, ppt1: 6, par1: 6, ppt2: 8, par2: 6,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 22, ppt1: 5, par1: 5, ppt2: 5, par2: 1,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 23, ppt1: 6, par1: 6, ppt2: 6, par2: 1,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 24, ppt1: 7, par1: 7, ppt2: 7, par2: 1,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 25, ppt1: 5, par1: 1, ppt2: 7, par2: 5,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 26, ppt1: 6, par1: 1, ppt2: 7, par2: 5,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 27, ppt1: 7, par1: 1, ppt2: 8, par2: 5,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 28, ppt1: 10, par1: 6, ppt2: 6, par2: 6,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 29, ppt1: 10, par1: 7, ppt2: 7, par2: 7,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 30, ppt1: 10, par1: 8, ppt2: 8, par2: 8,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 31, ppt1: 6, par1: 3, ppt2: 6, par2: 6,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 32, ppt1: 7, par1: 3, ppt2: 7, par2: 7,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 33, ppt1: 8, par1: 3, ppt2: 8, par2: 8,
-            Ac: 2, Phase: 1},
-          {ID: 'NA', Trial: 34, ppt1: 9, par1: 5, ppt2: 7, par2: 1,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 35, ppt1: 10, par1: 5, ppt2: 8, par2: 1,
-            Ac: 1, Phase: 1},
-          {ID: 'NA', Trial: 36, ppt1: 11, par1: 5, ppt2: 9, par2: 1,
-            Ac: 1, Phase: 1},
-        ]),
+        responses: JSON.stringify(requestResponses),
       },
     })
         .then((response) => {
