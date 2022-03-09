7d70a4a41e864873f21d8795ca9a299ff386af1b
Henry Burgess <henryjburg@gmail.com>
2022-03-09 10:55:02 -0600

    Add logging to R server

--- src/server/run.R
+++ src/server/run.R
@@ -1,5 +1,12 @@
+# Additional libraries
+library(logger)
 library(plumber)
 
+# Set the log level
+log_threshold(DEBUG)
+log_layout(layout_glue_colors)
+
+log_info("Starting server...")
 # 'server.R' is the location of the file that contains the API endpoint code
 pr("server.R") %>%
   pr_run(port = 8000, host = "0.0.0.0")
--- src/server/server.R
+++ src/server/server.R
@@ -1,7 +1,12 @@
+# Function file
 source("functions.R")
 
+# Additional libraries
 library(plumber)
 library(jsonlite)
+library(logger)
+
+# Set the log level
 
 #* Disable CORS for testing purposes only
 #* @filter cors
@@ -10,44 +15,60 @@ cors <- function(res) {
   plumber::forward()
 }
 
-#* Run a computation on the data gathered from phase one
+#* Primary API endpoint function
 #* @param req the request object
 #* @param id the participant ID
 #* @param responses the participant responses
 #* @get /compute/intentions
 function(req, id=0, responses="") {
-  valid_responses <- TRUE
+  # Flag for valid response
+  valid_request <- TRUE
+
+  if (id == 0) {
+    # The 'id' parameter was empty
+    valid_request <- FALSE
+    log_error("\'id\' parameter not specified!")
+  } else {
+    log_debug("Request from ID \'{id}\' received")
+  }
 
   if (responses == "") {
     # The 'responses' parameter was empty
-    valid_responses <- FALSE
+    valid_request <- FALSE
+    log_error("\'responses\' parameter not specified!")
   }
 
   # Declare the parsed responses
   parsed <- list()
-  if (valid_responses == TRUE) {
+  if (valid_request == TRUE) {
     # The 'responses' parameter has content, parse and check length
     parsed <- tryCatch(fromJSON(responses, simplifyDataFrame = TRUE),
                         error = function(e) {
+                          log_error("Error encountered while parsing \'responses\' content")
                           list()
                         })
   }
 
   # Generate a data frame from the responses
   computed <- list()
-  if (valid_responses == TRUE && length(parsed) > 0) {
+  if (valid_request == TRUE && length(parsed) > 0) {
+    log_debug("Valid request received, running function...")
     # Load the full data
     full_data <- read.csv("./data/fullData.csv") %>% dplyr::select(-X)
 
     # Create the partners
+    log_debug("Creating partners...")
     precan_df <- precan_partners(fullData)
 
     # Run the matching function
+    log_debug("Running matching function...")
     computed <- matching_partner_incremental_fit(
         phase1data = parsed,
         precan_df,
         shuffle = T,
         file_loc = F)
+  } else {
+    log_warn("Invalid request received, returning empty response")
   }
 
   # Respond to the game
