b43bbbc60f8f9fad4a976b441b58aed23788c0d4
Henry Burgess <henryjburg@gmail.com>
2022-03-17 10:09:09 -0500

    Add more fuzzing tests to API

--- api/tests/common.py
+++ api/tests/common.py
@@ -34,3 +34,85 @@ class Requests:
 
     # Assert that the responses are valid
     assert util.valid_responses(json.loads(content['computed'][0])) == True
+
+
+  # No ID
+  def no_id(address=""): 
+    # Send a request
+    logging.info("Sending request...")
+    response = util.create_request(address, params={
+      "responses": "[{\"ID\":\"NA\",\"Trial\":1,\"ppt1\":2,\"par1\":3,\"ppt2\":2,\"par2\":4,\"Ac\":1,\"Phase\":1}]"
+    })
+
+    # Extract the content of the response
+    content = None
+    try:
+      content = response.json()
+      logging.info("Request succeeded!")
+    except (json.JSONDecodeError):
+      logging.error("Error decoding JSON!")
+
+    # Assert that an HTTP error occurred
+    assert content is None
+
+
+  # No responses
+  def no_responses(address=""): 
+    # Send a request
+    logging.info("Sending request...")
+    response = util.create_request(address, params={
+      "id": 1234,
+    })
+
+    # Extract the content of the response
+    content = None
+    try:
+      content = response.json()
+      logging.info("Request succeeded!")
+    except (json.JSONDecodeError):
+      logging.error("Error decoding JSON!")
+
+    # Assert that an HTTP error occurred
+    assert content is None
+
+
+  # Invalid ID value
+  def invalid_id(address=""): 
+    # Send a request
+    logging.info("Sending request...")
+    response = util.create_request(address, params={
+      "id": "1l2n",
+      "responses": "[{\"ID\":\"NA\",\"Trial\":1,\"ppt1\":2,\"par1\":3,\"ppt2\":2,\"par2\":4,\"Ac\":1,\"Phase\":1}]"
+    })
+
+    # Extract the content of the response
+    content = None
+    try:
+      content = response.json()
+      logging.info("Request succeeded!")
+    except (json.JSONDecodeError):
+      logging.error("Error decoding JSON!")
+
+    # Assert that an HTTP error occurred
+    assert content is None
+
+
+  # Invalid responses value
+  def invalid_responses(address=""): 
+    # Send a request
+    logging.info("Sending request...")
+    response = util.create_request(address, params={
+      "id": 1234,
+      "responses": "[{\"ID:\"NA\",\"Trial\":1,\"ppt1\":2,\"par1\":3,\"ppt2\":2,\"par2\":4,\"Ac\":1,\"Phase\":1}]"
+    })
+
+    # Extract the content of the response
+    content = None
+    try:
+      content = response.json()
+      logging.info("Request succeeded!")
+    except (json.JSONDecodeError):
+      logging.error("Error decoding JSON!")
+
+    # Assert that an HTTP error occurred
+    assert content is None
--- api/tests/test_fuzzing.py
+++ api/tests/test_fuzzing.py
@@ -7,3 +7,19 @@ import common
 class TestFuzzing():
   def test_basic(self):
     util.runner(common.Requests.basic, args=(util.ADDR, ))
+
+
+  def test_no_id(self):
+    util.runner(common.Requests.no_id, args=(util.ADDR, ))
+
+
+  def test_no_responses(self):
+    util.runner(common.Requests.no_responses, args=(util.ADDR, ))
+
+
+  def test_invalid_id(self):
+    util.runner(common.Requests.invalid_id, args=(util.ADDR, ))
+
+
+  def test_invalid_responses(self):
+    util.runner(common.Requests.invalid_responses, args=(util.ADDR, ))
