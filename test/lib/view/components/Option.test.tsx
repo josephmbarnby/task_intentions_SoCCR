9b786ce2ade43de3852e4ae032f7aecf50f91996
Henry Burgess <henryjburg@gmail.com>
2022-03-23 13:59:15 -0500

    Add tests for Option component

--- test/lib/view/components/Option.test.tsx
+++ test/lib/view/components/Option.test.tsx
@@ -1,2 +1,47 @@
-// Option component test
-test.todo('Option component test');
+// Test utilities
+import {waitFor, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {axe, toHaveNoViolations} from 'jest-axe';
+import {act} from 'react-dom/test-utils';
+
+// React
+import React from 'react';
+
+// Custom wrapper
+import {render} from '../../Wrapper';
+
+// Option component
+import Option from '@components/Option';
+
+// Extend the 'expect' function
+expect.extend(toHaveNoViolations);
+
+test('loads and displays Option component', async () => {
+  await waitFor(() => render(
+      <Option
+        optionKey='test'
+        optionName='Option Test'
+        pointsParticipant={15}
+        pointsPartner={12}
+      />
+  ));
+
+  await waitFor(() => expect(screen.getByText('+15')).toBeInTheDocument());
+  await waitFor(() => expect(screen.getByText('+12')).toBeInTheDocument());
+});
+
+test('check Option component accessibility', async () => {
+  const {container} = render(
+      <Option
+        optionKey='test'
+        optionName='Option Test'
+        pointsParticipant={15}
+        pointsPartner={12}
+      />
+  );
+
+  await act(async () => {
+    const results = await axe(container);
+    await waitFor(() => expect(results).toHaveNoViolations());
+  });
+});
