dc548444676bc789dc1cac33baec0b99078c6faf
Henry Burgess <henryjburg@gmail.com>
2022-06-01 10:53:17 -0500

    Fix tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
+  setupFiles: ["jest-canvas-mock"],
   roots: ["test"],
   moduleNameMapper: {
     "\\.(css|scss)$": "<rootDir>/test/__mocks__/styles.js",
